13f38d1c70b81c8aa5660c993ae29d83ae3ffa64
Gabriele Pongelli <pongelligabriele@gmail.com>
2021-10-02 13:41:55 +0200

    Refactored the DialogWidgets module.
    - created the new abstract widget SingleOKButtonDialog
    - created the new WannaSaveDialog class

--- PySudoku/view/DialogWidgets.py
+++ PySudoku/view/DialogWidgets.py
@@ -8,9 +8,37 @@ from PyQt5.QtWidgets import (
     QWidget,
     QDialogButtonBox,
 )
+import PyQt5.QtCore as qtcore
 
 
-class DifficultyDialog(QDialog):
+class SingleOKButtonDialog(QDialog):
+    """Generic dialog with a single OK button."""
+
+    def _initButton(self, layout: QLayout = None) -> QLayout:
+        """Generate the button that will form this dialog.
+
+        Generate the button that will form this dialog and insert
+        it inside a layout.
+
+        Args:
+            layout (QLayout): layout in which to place the button. If is None
+            will be created a new QVBoxLayout. Defaults to None.
+
+        Returns:
+            QLayout: the resulting layout.
+        """
+
+        if layout is None:
+            layout = QVBoxLayout()
+
+        self._button = QDialogButtonBox(self)
+        self._button.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
+        layout.addWidget(self._button)
+
+        return layout
+
+
+class DifficultyDialog(SingleOKButtonDialog):
     """Dialog that allows to choose the difficulty level of the new game."""
 
     def __init__(
@@ -64,31 +92,8 @@ class DifficultyDialog(QDialog):
 
         return layout
 
-    def _initButton(self, layout: QLayout = None) -> QLayout:
-        """Generate the button that will form this dialog.
 
-        Generate the button that will form this dialog and insert
-        it inside a layout.
-
-        Args:
-            layout (QLayout): layout in which to place the button. If is None
-            will be created a new QVBoxLayout. Defaults to None.
-
-        Returns:
-            QLayout: the resulting layout.
-        """
-
-        if layout is None:
-            layout = QVBoxLayout()
-
-        self._button = QDialogButtonBox()
-        self._button.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
-        layout.addWidget(self._button)
-
-        return layout
-
-
-class ResultDialog(QDialog):
+class ResultDialog(SingleOKButtonDialog):
     """Dialog that communicate whether the sudoku has been solved or not."""
 
     def __init__(
@@ -138,11 +143,56 @@ class ResultDialog(QDialog):
 
         return layout
 
-    def _initButton(self, layout: QLayout = None) -> QLayout:
-        """Generate the button that will form this dialog.
 
-        Generate the button that will form this dialog and insert
-        it inside a layout.
+class WannaSaveDialog(QDialog):
+    """Dialog that let the user choose whether to save the game or not."""
+
+    def __init__(self, title: str, parent: Optional[QWidget] = None) -> None:
+        """Initialize the new widget.
+
+        Args:
+            title (str): title of the dialog.
+            parent (Optional[QWidget], optional): parent widget. Defaults to None.
+        """
+
+        super().__init__(parent)
+
+        self.setWindowTitle(title)
+        layout = QVBoxLayout()
+        self._initText(layout)
+        self._initButtons(layout)
+        self.setLayout(layout)
+
+        self._buttonBox.accepted.connect(self.accept)
+        self._buttonBox.rejected.connect(self.reject)
+
+    def _initText(self, layout: QLayout = None) -> QLayout:
+        """Generate the label containing the text.
+
+        Generate the label containing the tex and insert it inside a layout.
+
+        Args:
+            layout (QLayout): layout in which to place the combobox. If is
+            None will be created a new QVBoxLayout. Defaults to None.
+
+        Returns:
+            QLayout: the resulting layout.
+        """
+
+        if layout is None:
+            layout = QVBoxLayout()
+
+        self.label = QLabel("Do you want to save the game?", self)
+        self.label.setAlignment(qtcore.Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.label)
+
+        return layout
+
+    def _initButtons(self, layout: QLayout = None) -> QLayout:
+        """Generate the buttons that will form this dialog.
+
+        Generate the buttons that will form this dialog and insert
+        them inside a layout.
 
         Args:
             layout (QLayout): layout in which to place the button. If is None
@@ -155,8 +205,11 @@ class ResultDialog(QDialog):
         if layout is None:
             layout = QVBoxLayout()
 
-        self._button = QDialogButtonBox()
-        self._button.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
-        layout.addWidget(self._button)
+        self._buttonBox = QDialogButtonBox(
+            QDialogButtonBox.StandardButton.Save | QDialogButtonBox.StandardButton.No,
+            self,
+        )
+
+        layout.addWidget(self._buttonBox)
 
         return layout
